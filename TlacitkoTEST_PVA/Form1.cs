554148ebeeabea08f00d06748de7281fcde498bf
Jakub.Grezl <jakub.grezl@outlook.cz>
2024-05-15 12:08:48 +0200

    before PocitaciTlacitko

--- TlacitkoTEST_PVA/Form1.cs
+++ TlacitkoTEST_PVA/Form1.cs
@@ -6,5 +6,7 @@ namespace TlacitkoTEST_PVA
         {
             InitializeComponent();
         }
+
+
     }
 }
---
+++ TlacitkoTEST_PVA/ZakladniButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TlacitkoTEST_PVA
+{
+    public abstract class ZakladniButton : Button
+    {
+        protected static int pocetKliknuti;
+
+        protected static Action<int> ZmenPocetKliknuti;
+
+        protected ZakladniButton()
+        {
+            ZmenPocetKliknuti += ZmenPocetKliknutiMetoda;
+        }
+
+        private void ZmenPocetKliknutiMetoda(int obj)
+        {
+            pocetKliknuti = obj;
+            ReakceNaZmenuPoctu();
+        }
+
+        protected abstract void TlacitkoZakliknuto();
+
+        protected void ReakceNaZmenuPoctu()
+        {
+
+        }
+    }
+}
